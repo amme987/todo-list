75cd9e3ad84b2422a2f05d1701ea4b5f1ae3681d
amme987 <emmagination97@gmail.com>
2023-10-03 11:12:51 -0400

    Add form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./style.css";
-
 // todo: design popup for adding todo to list
+const addTask = document.querySelector(".add");
+const overlay = document.querySelector(".overlay");
+addTask.addEventListener("click", openForm);
+overlay.addEventListener("click", closeForm);
+
+function openForm() {
+  document.querySelector("form").style.display = "flex";
+  overlay.style.display = "block";
+}
+
+function closeForm() {
+  document.querySelector("form").style.display = "none";
+  overlay.style.display = "none";
+}
